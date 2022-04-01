51bc548c30352251fdbbe45881f09aaca546f1bf
Abdullah Zübeyir Yıldız <35402428+abdullahzubeyiryildiz@users.noreply.github.com>
2022-04-01 16:10:24 +0300

    Update edit.blade.php

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -68,7 +68,7 @@
         </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Kaydet</button>
     </div>
 </div>
 </form>
